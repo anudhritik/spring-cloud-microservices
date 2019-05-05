a099282bd9a2bbb376c53f43cd2ee7a6e075ec60
anudhritik <anukreddy28@gmail.com>
2019-05-04 23:22:20 -0400

    Adding limits-service

---
+++ limits-service/src/main/java/com/demo/microservices/limitsservice/Configuration.java
@@ -0,0 +1,28 @@
+package com.demo.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	private int minimum;
+	private int maximum;
+
+	public int getMinium() {
+		return minimum;
+	}
+
+	public void setMinium(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
